050f414e2e29a8610473f3c8e9536b6df1bef179
Mayur Dayal <79074393+mayurDayal2000@users.noreply.github.com>
2021-02-19 10:14:06 +0530

    lesson 02

---
+++ Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,34 @@
+/* 
+In this challenge, you must read integers from stdin and then print them to stdout. Each integer must be printed on a new line. 
+To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Sample Input
+
+42
+100
+125
+
+Sample Output
+
+42
+100
+125
+
+*/
+
+import java.util.Scanner;
+class Solution{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        scan.close();
+    }
+}
